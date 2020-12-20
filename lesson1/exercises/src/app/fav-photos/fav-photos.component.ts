624547ccbfa2981d2978d8cd62b1aa015deef592
lindseyms <51429410+lindseyms@users.noreply.github.com>
2020-12-20 15:06:49 -0600

    updated styling

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i1.wp.com/ukinterns.society19.com/wp-content/uploads/2018/08/366571dcd1cdff56c98ea61e5be59e5e.jpg?resize=564%2C564&ssl=1';
+  image3 = 'https://i1.wp.com/ukinterns.society19.com/wp-content/uploads/2018/08/76710bbd900951cbc5a1fa8792659b06.jpg?resize=564%2C564&ssl=1';
 
   constructor() { }
 
